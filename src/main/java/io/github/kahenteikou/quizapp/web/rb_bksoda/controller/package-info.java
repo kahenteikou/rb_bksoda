b9a11528afa37bd7a595389a4ca05c939b3b8d2f
kahenteikou <kokkiemouse@gmail.com>
2023-05-16 06:14:31 +0900

    x

---
+++ src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/controller/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * controller package for the rb_bksoda web service
+ */
+package io.github.kahenteikou.quizapp.web.rb_bksoda.controller;
---
+++ src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/controller/v1/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * ver 1
+ */
+package io.github.kahenteikou.quizapp.web.rb_bksoda.controller.v1;
