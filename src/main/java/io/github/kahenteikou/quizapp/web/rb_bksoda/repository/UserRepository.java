644d3a265f5444165736f638a7ce93c26274446e
kahenteikou <kokkiemouse@gmail.com>
2023-05-16 06:13:52 +0900

    z2

---
+++ src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package io.github.kahenteikou.quizapp.web.rb_bksoda.repository;
+
+import io.github.kahenteikou.quizapp.web.rb_bksoda.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
---
+++ src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/repository/package-info.java
@@ -0,0 +1,1 @@
+package io.github.kahenteikou.quizapp.web.rb_bksoda.repository;
