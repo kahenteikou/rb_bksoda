0b93e796b0340197ddc9161504d72ef2508694ed
kahenteikou <kokkiemouse@gmail.com>
2023-05-17 06:51:22 +0900

    5

---
+++ src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/controller/v1/Question_SetController.java
@@ -0,0 +1,8 @@
+package io.github.kahenteikou.quizapp.web.rb_bksoda.controller.v1;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/v1/question_sets")
+public class Question_SetController {
+}
---
+++ src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/repository/Question_SetRepository.java
@@ -0,0 +1,7 @@
+package io.github.kahenteikou.quizapp.web.rb_bksoda.repository;
+
+import io.github.kahenteikou.quizapp.web.rb_bksoda.model.Question_Set;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Question_SetRepository extends JpaRepository<Question_Set,Long> {
+}
