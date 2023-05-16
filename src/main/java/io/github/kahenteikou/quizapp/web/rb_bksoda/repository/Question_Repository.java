b241396e9097df8fbeb7b78aad8f61c36f6fd805
kahenteikou <kokkiemouse@gmail.com>
2023-05-17 07:10:40 +0900

    4

---
+++ src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/repository/Question_Repository.java
@@ -0,0 +1,7 @@
+package io.github.kahenteikou.quizapp.web.rb_bksoda.repository;
+
+import io.github.kahenteikou.quizapp.web.rb_bksoda.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Question_Repository extends JpaRepository<Question,Long> {
+}
