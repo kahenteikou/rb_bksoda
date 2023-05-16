91f37432abe338639db88e11929946e943258db0
kahenteikou <kokkiemouse@gmail.com>
2023-05-17 06:56:23 +0900

    65

--- src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/controller/v1/Question_SetController.java
+++ src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/controller/v1/Question_SetController.java
@@ -1,8 +1,55 @@
 package io.github.kahenteikou.quizapp.web.rb_bksoda.controller.v1;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import io.github.kahenteikou.quizapp.web.rb_bksoda.model.Question_Set;
+import io.github.kahenteikou.quizapp.web.rb_bksoda.model.User;
+import io.github.kahenteikou.quizapp.web.rb_bksoda.repository.Question_SetRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("api/v1/question_sets")
+@RequiredArgsConstructor
+@RestController
 public class Question_SetController {
+    @NotNull
+    private final Question_SetRepository question_setRepository;
+
+    @Operation(summary = "Get all sets")
+    @GetMapping("/")
+    List<Question_Set> findAll(){
+        return question_setRepository.findAll();
+    }
+
+    @Operation(summary = "Create a new question set")
+    @PostMapping("/")
+    Question_Set save(@RequestBody Question_Set qs){
+        return question_setRepository.save(qs);
+    }
+
+    @Operation(summary = "Get a question set by id")
+    @GetMapping("/{id}")
+    Question_Set findById(@PathVariable Long id){
+        return question_setRepository.findById(id).get();
+    }
+    @Operation(summary = "Update question set")
+    @PutMapping("/{id}")
+    Question_Set save(@RequestBody Question_Set newqs,@PathVariable Long id){
+        return question_setRepository.findById(id).map(qs->{
+            qs.setQuestionset_name(newqs.getQuestionset_name());
+            return question_setRepository.save(qs);
+        }).orElseGet(()->{
+            newqs.setId(id);
+            return question_setRepository.save(newqs);
+        });
+    }
+    @Operation(summary = "Delete a question set by id")
+    @DeleteMapping("/{id}")
+    void deleteById(@PathVariable Long id){
+        question_setRepository.deleteById(id);
+    }
+
 }
