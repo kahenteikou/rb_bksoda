74aebfcb80cb4e822284038bd370ce2741308957
kahenteikou <kokkiemouse@gmail.com>
2023-05-30 07:21:45 +0900

    v

--- ClientApp/src/pages/UserManagerPage.tsx
+++ ClientApp/src/pages/UserManagerPage.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { useAllUsers } from '../hooks/useAllUsers';
 import { useEffect, useState } from 'react';
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Container, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import { User } from '../types/User';
 
 export default function UserManagerPage(): React.ReactElement {
-    const {getAllUsers,users}=useAllUsers();
+    const { getAllUsers, users } = useAllUsers();
     const ModalStyle = {
         content: {
             top: "20%",
@@ -19,26 +19,27 @@ export default function UserManagerPage(): React.ReactElement {
             maxWidth: "50%",
         },
     }
-    const [editModalIsOpen,seteditModalIsOpen]=useState(false);
-    const [selectedUser,setselectedUser]=useState<User>();
-    const openEditmodal=(user:User)=>{
+    const [editModalIsOpen, seteditModalIsOpen] = useState(false);
+    const [selectedUser, setselectedUser] = useState<User>();
+    const openEditmodal = (user: User) => {
         setselectedUser(user);
         seteditModalIsOpen(true);
     };
-    useEffect(()=>{
+    useEffect(() => {
         getAllUsers();
-    },[]);
-    function post_edited_value_and_refresh(){
-        console.log("after log:",selectedUser);
+    }, []);
+    function post_edited_value_and_refresh() {
+        console.log("after log:", selectedUser);
         closeModal();
     }
-    const closeModal=()=>{
+    const closeModal = () => {
         seteditModalIsOpen(false);
     }
     return (
         <>
-        <h1>UserManagerPage</h1><br/>
-        {/*
+            <Container maxWidth="sm">
+                <h1>UserManagerPage</h1><br />
+                {/*
         <ul>
             {
                 users.map((user)=>
@@ -48,48 +49,49 @@ export default function UserManagerPage(): React.ReactElement {
                 </>)
             }
         </ul>*/}
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth:650}} aria-label="table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>
-                            Name
-                        </TableCell>
-                        <TableCell align="right">
-                            displayname
-                        </TableCell>
-                        <TableCell align="right">
-                            UUID
-                        </TableCell>
-                        <TableCell align="right">
-                            description
-                        </TableCell>
-                        <TableCell align="right">
-                            Edit
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {users.map((user)=>(
-                        <TableRow key={user.uuid}>
-                            <TableCell component="th" scope="row">
-                                {user.username}
-                            </TableCell>
-                            <TableCell align="right">{user.displayname}</TableCell>
-                            <TableCell align="right">{user.uuid}</TableCell>
-                            <TableCell align="right">{user.description}</TableCell>
-                            <TableCell align="right">
-                                <IconButton aria-label="edit" onClick={()=>{
-                                    console.log("edit: %s",user.uuid );
-                                }}>
-                                    <EditIcon />
-                                </IconButton>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>
+                                    Name
+                                </TableCell>
+                                <TableCell align="right">
+                                    displayname
+                                </TableCell>
+                                <TableCell align="right">
+                                    UUID
+                                </TableCell>
+                                <TableCell align="right">
+                                    description
+                                </TableCell>
+                                <TableCell align="right">
+                                    Edit
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {users.map((user) => (
+                                <TableRow key={user.uuid}>
+                                    <TableCell component="th" scope="row">
+                                        {user.username}
+                                    </TableCell>
+                                    <TableCell align="right">{user.displayname}</TableCell>
+                                    <TableCell align="right">{user.uuid}</TableCell>
+                                    <TableCell align="right">{user.description}</TableCell>
+                                    <TableCell align="right">
+                                        <IconButton aria-label="edit" onClick={() => {
+                                            console.log("edit: %s", user.uuid);
+                                        }}>
+                                            <EditIcon />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
         </>
     );
 }
