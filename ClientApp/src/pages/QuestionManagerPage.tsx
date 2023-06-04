706b28a9033fae333fb30ffabb6ac839e41a0a72
kahenteikou <kokkiemouse@gmail.com>
2023-06-04 10:39:55 +0900

    4

--- ClientApp/src/pages/QuestionManagerPage.tsx
+++ ClientApp/src/pages/QuestionManagerPage.tsx
@@ -16,6 +16,26 @@ export default function QuestionManagerPage(): React.ReactElement {
     const tab_HandleChange=(event: React.SyntheticEvent, newValue: number)=>{
         setTabIndex(newValue);
     };
+    interface TabPanelProps{
+        children?:React.ReactNode;
+        index:number;
+        value:number;
+    }
+    function TabPanel(props:TabPanelProps){
+        const {children,value,index,...other}=props;
+        return(
+            <div role="tabpanel" hidden={value!==index}
+            id={`question-add-tabpanel-${index}`}
+            aria-labelledby={`question-add-tab-${index}`}
+            {...other}>
+                {value===index&&(
+                    <Box sx={{p:3}}>
+                        {children}
+                    </Box>
+                )}
+            </div>
+        );
+    }
     useEffect(()=>{
         getAllQuestions();
     },[]);
