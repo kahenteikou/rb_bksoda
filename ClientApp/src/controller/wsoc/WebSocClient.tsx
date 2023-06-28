9d2437e0336825ec0f0dcffd94bbe7fd876401dc
kahenteikou <kokkiemouse@gmail.com>
2023-06-29 07:28:07 +0900

    cx

---
+++ ClientApp/src/controller/wsoc/WebSocClient.tsx
@@ -0,0 +1,12 @@
+type SubScriber<T=unknown>=(message:T)=>void|(()=>void)
+type WebSocClientConf={
+    urls:string[]
+}
+export class WebSocClient{
+    private readonly conf:WebSocClientConf;
+    private readonly subscribers=new Map<string,Set<SubScriber>>()
+    //private readonly wsocs=new Map<string,Re
+    constructor(config:WebSocClientConf){
+        this.conf=config;
+    }
+}
