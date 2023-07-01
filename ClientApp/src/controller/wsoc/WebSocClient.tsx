fbf8250bc323ec5c87b091e9343a2480d7bac590
kahenteikou <kokkiemouse@gmail.com>
2023-07-02 08:44:34 +0900

    c

--- ClientApp/src/controller/wsoc/WebSocClient.tsx
+++ ClientApp/src/controller/wsoc/WebSocClient.tsx
@@ -1,3 +1,5 @@
+import ReconnectingWebSocket from "reconnecting-websocket";
+
 type SubScriber<T=unknown>=(message:T)=>void|(()=>void)
 type WebSocClientConf={
     urls:string[]
@@ -5,7 +7,7 @@ type WebSocClientConf={
 export class WebSocClient{
     private readonly conf:WebSocClientConf;
     private readonly subscribers=new Map<string,Set<SubScriber>>()
-    //private readonly wsocs=new Map<string,Re
+    private readonly wsocs=new Map<string,ReconnectingWebSocket>();
     constructor(config:WebSocClientConf){
         this.conf=config;
     }
