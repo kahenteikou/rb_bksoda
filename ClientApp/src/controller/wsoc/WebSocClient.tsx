05bd9bc7d8aad707b896b02f7ea79569cf5a90b9
kahenteikou <kokkiemouse@gmail.com>
2023-07-02 17:12:28 +0900

    c

--- ClientApp/src/controller/wsoc/WebSocClient.tsx
+++ ClientApp/src/controller/wsoc/WebSocClient.tsx
@@ -8,7 +8,24 @@ export class WebSocClient{
     private readonly conf:WebSocClientConf;
     private readonly subscribers=new Map<string,Set<SubScriber>>()
     private readonly wsocs=new Map<string,ReconnectingWebSocket>();
+    private data:{[url:string]:unknown}|undefined;
     constructor(config:WebSocClientConf){
         this.conf=config;
+        const{urls}=config;
+        urls.forEach((url)=>{
+            this.subscribers.set(url,new Set());
+        });
+    }
+    open=()=>{
+        const{urls}=this.conf;
+        urls.forEach((url)=>{
+            const ws=new ReconnectingWebSocket(url);
+            ws.addEventListener("message",(ev:MessageEvent<string>)=>{
+                const pData=JSON.parse(ev.data);
+                this.data={...this.data,[url]:pData};
+                this.subscribers.get(url)?.forEach((s)=>s(pData));
+            });
+            this.wsocs.set(url,ws);
+        });
     }
 }
