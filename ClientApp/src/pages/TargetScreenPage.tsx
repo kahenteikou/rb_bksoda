df0ac3f7070df6017b617ade2a73ea6718b58560
kahenteikou <kokkiemouse@gmail.com>
2023-07-03 07:24:05 +0900

    2

--- ClientApp/src/pages/TargetScreenPage.tsx
+++ ClientApp/src/pages/TargetScreenPage.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import { WebSocClient } from '../controller/wsoc/WebSocClient';
 
 export default function TargetScreenPage():React.ReactElement{
+    const client = new WebSocClient({
+        urls:["ws://localhost:8080/api/v1/websocket/msg_1"]
+    })
     return (
+            
         <>
             Target Screen
         </>
