1a20b516d194307aff16aa77c4f3bdd69a338cf4
kahenteikou <kokkiemouse@gmail.com>
2023-07-04 07:32:05 +0900

    CX

--- src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/WebSocketConfig.java
+++ src/main/java/io/github/kahenteikou/quizapp/web/rb_bksoda/WebSocketConfig.java
@@ -17,6 +17,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/wsoc_tools").withSockJS();
+        registry.addEndpoint("/wsoc_tools")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 }
